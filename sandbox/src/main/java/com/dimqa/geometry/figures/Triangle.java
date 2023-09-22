bce96e5614b6abcc833f9a26f6408591da1ddfbc
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-09-22 20:42:09 +0500

    Задание №4: Потренироваться использовать сравнение объектов

--- sandbox/src/main/java/com/dimqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/com/dimqa/geometry/figures/Triangle.java
@@ -1,5 +1,7 @@
 package com.dimqa.geometry.figures;
 
+import java.util.Objects;
+
 public record Triangle(double a, double b, double c) {
 
     public Triangle {
@@ -18,5 +20,23 @@ public record Triangle(double a, double b, double c) {
     public double area() {
         double p = perimeter() / 2;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return (Double.compare(a, triangle.a) == 0 && Double.compare(b, triangle.b) == 0 && Double.compare(c, triangle.c) == 0)
+                || (Double.compare(b, triangle.a) == 0 && Double.compare(c, triangle.b) == 0 && Double.compare(a, triangle.c) == 0)
+                || (Double.compare(c, triangle.a) == 0 && Double.compare(a, triangle.b) == 0 && Double.compare(b, triangle.c) == 0)
+                || (Double.compare(a, triangle.a) == 0 && Double.compare(c, triangle.b) == 0 && Double.compare(b, triangle.c) == 0)
+                || (Double.compare(c, triangle.a) == 0 && Double.compare(b, triangle.b) == 0 && Double.compare(a, triangle.c) == 0)
+                || (Double.compare(b, triangle.a) == 0 && Double.compare(a, triangle.b) == 0 && Double.compare(c, triangle.c) == 0);
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
     }
 }
--- sandbox/src/test/java/com/dimqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/com/dimqa/geometry/figures/TriangleTests.java
@@ -76,4 +76,53 @@ public class TriangleTests {
             // OK
         }
     }
+
+    @Test
+    void testEquality() {
+        Triangle t1 = new Triangle(3, 4, 5);
+        Triangle t2 = new Triangle(3, 4, 5);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testNonEquality() {
+        Triangle t1 = new Triangle(3, 4, 5);
+        Triangle t2 = new Triangle(4, 5, 6);
+        Assertions.assertNotEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality2() {
+        Triangle t1 = new Triangle(3, 4, 5);
+        Triangle t2 = new Triangle(4, 5, 3);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality3() {
+        Triangle t1 = new Triangle(3, 4, 5);
+        Triangle t2 = new Triangle(5, 3, 4);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality4() {
+        Triangle t1 = new Triangle(3, 4, 5);
+        Triangle t2 = new Triangle(3, 5, 4);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality5() {
+        Triangle t1 = new Triangle(3, 4, 5);
+        Triangle t2 = new Triangle(5, 4, 3);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality6() {
+        Triangle t1 = new Triangle(3, 4, 5);
+        Triangle t2 = new Triangle(4, 3, 5);
+        Assertions.assertEquals(t1, t2);
+    }
 }
