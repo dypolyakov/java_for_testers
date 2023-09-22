6514cf82f53c09f4c165a2c69974fda1c2b5893d
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-09-22 19:55:53 +0500

    Задание №3: Потренироваться использовать ветвления и исключения

--- sandbox/src/main/java/com/dimqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/com/dimqa/geometry/figures/Triangle.java
@@ -1,6 +1,16 @@
 package com.dimqa.geometry.figures;
 
 public record Triangle(double a, double b, double c) {
+
+    public Triangle {
+        if (a < 0 || b < 0 || c < 0) {
+            throw new IllegalArgumentException("Triangle side should be non-negative");
+        }
+        if (a + b < c || a + c < b || b + c < a) {
+            throw new IllegalArgumentException("The sum of any two sides of a triangle " +
+                    "must be at least as large as the third side of the triangle");
+        }
+    }
     public double perimeter() {
         return a + b + c;
     }
--- sandbox/src/test/java/com/dimqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/com/dimqa/geometry/figures/TriangleTests.java
@@ -16,4 +16,64 @@ public class TriangleTests {
         Triangle triangle = new Triangle(3.0, 5.0, 7.0);
         Assertions.assertEquals(6.49519052838329, triangle.area());
     }
+
+    @Test
+    void cannotCreateTriangleWithNegativeSideA() {
+        try {
+            new Triangle(-1.0, 2.0, 3.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
+
+    @Test
+    void cannotCreateTriangleWithNegativeSideB() {
+        try {
+            new Triangle(1.0, -2.0, 3.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
+
+    @Test
+    void cannotCreateTriangleWithNegativeSideC() {
+        try {
+            new Triangle(1.0, 2.0, -3.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
+
+    @Test
+    void cannotCreateTriangleWithSidesAandBLessThenC() {
+        try {
+            new Triangle(5.0,6.0,12.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
+
+    @Test
+    void cannotCreateTriangleWithSidesAandCLessThenB() {
+        try {
+            new Triangle(5.0,18.0,12.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
+
+    @Test
+    void cannotCreateTriangleWithSidesBandCLessThenA() {
+        try {
+            new Triangle(31.0,18.0,12.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
 }
