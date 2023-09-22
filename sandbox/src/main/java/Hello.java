8f5662570119705688df618732dc3cf56ad739b3
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-09-22 19:30:17 +0500

    Добавлен код из лекции "2.3. Ветвление (конструкция if-then-else)"

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,20 +1,13 @@
-import java.io.File;
-
 public class Hello {
     public static void main(String[] args) {
-        try {
-            int z = calculate();
-            System.out.println(z);
-        } catch (ArithmeticException exception) {
-            System.out.println(exception.getMessage());
-        }
-    }
-
-    private static int calculate() {
         int x = 1;
         int y = 0;
-        int z = divide(x, y);
-        return z;
+        if (y == 0) {
+            System.out.println("Division by zero is not allowed");
+        } else {
+            int z = divide(x, y);
+            System.out.printf("%d / %d = %d", x, y, z);
+        }
     }
 
     private static int divide(int x, int y) {
--- sandbox/src/main/java/com/dimqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/com/dimqa/geometry/figures/Rectangle.java
@@ -1,6 +1,12 @@
 package com.dimqa.geometry.figures;
 
 public record Rectangle(double a, double b) {
+
+    public Rectangle {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("Rectangle side should be non-negative");
+        }
+    }
     public static void printRectangleArea(double a, double b) {
         System.out.printf("Площадь прямоугольника со сторонами %.1f и %.1f = %.1f%n", a, b, rectangleArea(a, b));
     }
