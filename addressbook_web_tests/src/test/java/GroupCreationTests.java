56e89ec89f65e4964547048763742a457edbf552
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-09-29 17:18:26 +0500

    Добавлен новый класс GroupData

--- addressbook_web_tests/src/test/java/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -1,3 +1,4 @@
+import model.GroupData;
 import org.junit.jupiter.api.Test;
 
 public class GroupCreationTests extends TestBase {
@@ -5,13 +6,19 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void canCreateGroup() {
         openGroupsPage();
-        createGroup("Group name", "Group header", "Group footer");
+        createGroup(new GroupData("Group name", "Group header", "Group footer"));
     }
 
     @Test
     public void canCreateGroupWithEmptyName() {
         openGroupsPage();
-        createGroup("", "", "");
+        createGroup(new GroupData());
+    }
+
+    @Test
+    public void canCreateGroupWithNameOnly() {
+        openGroupsPage();
+        createGroup(new GroupData().withName("Some name"));
     }
 
 }
