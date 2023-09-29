75b470cac43ef49c632df2926821cce0c782ff61
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-09-29 18:50:03 +0500

    Создана двухуровневая архитектура тестового набора

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -1,22 +1,16 @@
+package manager;
+
 import model.GroupData;
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TestBase {
+public class ApplicationManager {
     protected static WebDriver driver;
 
-    protected static void removeGroup() {
-        driver.findElement(By.name("selected[]")).click();
-        driver.findElement(By.name("delete")).click();
-        driver.findElement(By.linkText("group page")).click();
-    }
-
-    @BeforeEach
-    public void setUp() {
+    public void init() {
         if (driver == null) {
             driver = new ChromeDriver();
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
@@ -37,7 +31,7 @@ public class TestBase {
         }
     }
 
-    protected void createGroup(GroupData group) {
+    public void createGroup(GroupData group) {
         driver.findElement(By.name("new")).click();
         driver.findElement(By.name("group_name")).sendKeys(group.name());
         driver.findElement(By.name("group_header")).sendKeys(group.header());
@@ -46,13 +40,19 @@ public class TestBase {
         driver.findElement(By.linkText("group page")).click();
     }
 
-    protected void openGroupsPage() {
+    public void removeGroup() {
+        driver.findElement(By.name("selected[]")).click();
+        driver.findElement(By.name("delete")).click();
+        driver.findElement(By.linkText("group page")).click();
+    }
+
+    public void openGroupsPage() {
         if (!isElementPresent(By.name("new"))) {
             driver.findElement(By.linkText("groups")).click();
         }
     }
 
-    protected boolean isGroupPresent() {
+    public boolean isGroupPresent() {
         return isElementPresent(By.name("selected[]"));
     }
 }
---
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -0,0 +1,26 @@
+package tests;
+
+import model.GroupData;
+import org.junit.jupiter.api.Test;
+
+public class GroupCreationTests extends TestBase {
+
+    @Test
+    public void canCreateGroup() {
+        app.openGroupsPage();
+        app.createGroup(new GroupData("Group name", "Group header", "Group footer"));
+    }
+
+    @Test
+    public void canCreateGroupWithEmptyName() {
+        app.openGroupsPage();
+        app.createGroup(new GroupData());
+    }
+
+    @Test
+    public void canCreateGroupWithNameOnly() {
+        app.openGroupsPage();
+        app.createGroup(new GroupData().withName("Some name"));
+    }
+
+}
