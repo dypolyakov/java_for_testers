f085865e411f02655bd8c96c631a031abcd89133
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-10-06 21:13:26 +0500

    Тест удаления контакта: добавлено сравнение списка контактов

--- addressbook_web_tests/src/test/java/manager/ContactHelper.java
+++ addressbook_web_tests/src/test/java/manager/ContactHelper.java
@@ -2,6 +2,10 @@ package manager;
 
 import model.ContactData;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactHelper extends HelperBase {
 
@@ -42,9 +46,9 @@ public class ContactHelper extends HelperBase {
         }
     }
 
-    public void removeContact() {
+    public void removeContact(ContactData contact) {
         openContactsPage();
-        selectContact();
+        selectContact(contact);
         removeSelectedContact();
         confirmRemoval();
         openContactsPage();
@@ -58,11 +62,33 @@ public class ContactHelper extends HelperBase {
         click(By.xpath("//input[@value='Delete']"));
     }
 
-    private void selectContact() {
-        click(By.name("selected[]"));
+    private void selectContact(ContactData contact) {
+        click(By.id(contact.id()));
     }
 
     public int getCount() {
         return manager.driver.findElements(By.name("selected[]")).size();
+    }
+
+    public List<ContactData> getList() {
+        List<ContactData> contacts = new ArrayList<>();
+        List<WebElement> elements = manager.driver.findElements(By.name("entry"));
+        for (WebElement element : elements) {
+            String id = element.findElement(By.name("selected[]")).getAttribute("id");
+            String firstName = element.findElement(By.xpath("td[3]")).getText();
+            String lastName = element.findElement(By.xpath("td[2]")).getText();
+            String address = element.findElement(By.xpath("td[4]")).getText();
+            String email = element.findElement(By.xpath("td[5]")).getText();
+            String phone = element.findElement(By.xpath("td[6]")).getText();
+            contacts.add(new ContactData()
+                    .withId(id)
+                    .withFirstName(firstName)
+                    .withLastName(lastName)
+                    .withAddress(address)
+                    .withEmail(email)
+                    .withPhone(phone)
+            );
+        }
+        return  contacts;
     }
 }
--- addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
@@ -4,16 +4,23 @@ import model.ContactData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class ContactRemovalTests extends TestBase {
 
     @Test
     public void canRemoveContact() {
-        if(app.contacts().getCount() == 0) {
+        if (app.contacts().getCount() == 0) {
             app.contacts().createContact(new ContactData().withFirstName("For removal"));
         }
-        int contactsCount = app.contacts().getCount();
-        app.contacts().removeContact();
-        int newContactsCount = app.contacts().getCount();
-        Assertions.assertEquals(contactsCount - 1, newContactsCount);
+        List<ContactData> oldContacts = app.contacts().getList();
+        int index = new Random().nextInt(oldContacts.size());
+        app.contacts().removeContact(oldContacts.get(index));
+        List<ContactData> newContacts = app.contacts().getList();
+        List<ContactData> expectedContacts = new ArrayList<>(oldContacts);
+        expectedContacts.remove(index);
+        Assertions.assertEquals(expectedContacts, newContacts);
     }
 }
