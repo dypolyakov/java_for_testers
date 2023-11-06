f77de5c173f916ffa192b685405bf0ef6f3cad6a
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-11-06 18:44:33 +0300

    Вместо 3-х разных тестов реализован 1, который для выбранного контакта проверяет телефоны, почту и адреса.

--- addressbook_web_tests/src/test/java/tests/ContactInfoTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactInfoTests.java
@@ -4,34 +4,27 @@ import model.ContactData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ContactInfoTests extends TestBase {
-
     @Test
-    public void testPhones() {
-        List<ContactData> contacts = app.hbm().getContactList();
-        ContactData contact = contacts.get(0);
+    public void testContact() {
+        ContactData contact = app.hbm().getContactList().get(0);
+
         String phones = app.contacts().getPhones(contact);
-        String expected = Stream.of(contact.home(), contact.mobile(), contact.work(), contact.secondary()).filter(s -> s != null && !s.isEmpty()).collect(Collectors.joining("\n"));
-        Assertions.assertEquals(expected, phones);
-    }
+        String expectedPhones = Stream.of(contact.home(), contact.mobile(), contact.work(), contact.secondary())
+                .filter(s -> s != null && !s.isEmpty()).collect(Collectors.joining("\n"));
+        Assertions.assertEquals(expectedPhones, phones);
 
-    @Test
-    public void testEmails() {
-        ContactData contact = app.hbm().getContactList().get(0);
         String emails = app.contacts().getEmails(contact);
-        String expectedEmails = Stream.of(contact.email(), contact.email2(), contact.email3()).filter(s -> s != null && !s.isEmpty()).collect(Collectors.joining("\n"));
+        String expectedEmails = Stream.of(contact.email(), contact.email2(), contact.email3())
+                .filter(s -> s != null && !s.isEmpty()).collect(Collectors.joining("\n"));
         Assertions.assertEquals(expectedEmails, emails);
-    }
 
-    @Test
-    public void testAddress() {
-        ContactData contact = app.hbm().getContactList().get(0);
         String address = app.contacts().getAddress(contact);
-        String expectedAddress = Stream.of(contact.address(), contact.address2()).filter(s -> s != null && !s.isEmpty()).collect(Collectors.joining("\n"));
+        String expectedAddress = Stream.of(contact.address(), contact.address2())
+                .filter(s -> s != null && !s.isEmpty()).collect(Collectors.joining("\n"));
         Assertions.assertEquals(expectedAddress, address);
     }
 }
